2abc8ba66000b335735a7e9a96324e72d41b5796
aktssam <aktssam.alfarisi@gmail.com>
2023-11-22 18:01:30 +0700

    Update about content

--- app/about/components/Description.tsx
+++ app/about/components/Description.tsx
@@ -5,22 +5,34 @@ export default function Description() {
     <div className="w-full self-end mt-10 md:mb-auto md:mt-0 md:w-3/5">
       <section className="text-center md:text-left mb-8">
         <h2 className="text-2xl font-semibold mb-4 pb-3 border-b">About me</h2>
-        <p className=" text-xl ">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam saepe, aliquid odit deserunt exercitationem culpa totam, quisquam voluptates earum iste amet repellendus nemo in tempora minus quo laborum? Cumque ab tenetur libero saepe exercitationem consequatur eius nihil sapiente quam, assumenda tempore, at laboriosam recusandae! Molestiae molestias totam repudiandae est eius!
+        <p className="text-xl">
+          I’m a fresh graduate of Informatics Department and I have gained experience in web programming, having worked as an intern in a Flutter development for 3 month. Loved to exploring new technologies and currently learning NextJS. I am a proactive, responsible, and motivated individual to learn and grow.
+
         </p>
       </section>
-      <section className="text-left">
+      <section className="text-left mb-8">
         <h2 className="text-2xl font-semibold mb-4 pb-3 border-b">Educations</h2>
-        <div className="flex justify-between">
+        <div className="flex flex-wrap justify-between">
           <div>
-            <h2 className="text-lg font-medium pb-1 md:text-xl">Universitas Internasional Semen Indonesia</h2>
-            <p className="text-sm text-gray-500 md:text-lg">Bachelor Degree - Informatika</p>
+            <h2 className="text-lg font-medium pb-1 md:text-xl self-start">Universitas Internasional Semen Indonesia</h2>
+            <p className="w-full text-md text-gray-500 md:text-lg self-end">Bachelor Degree - Informatika</p>
           </div>
           <p className="text-sm text-gray-500 md:text-lg">
             2018-2023
           </p>
         </div>
-
+      </section>
+      <section className="text-left">
+        <h2 className="text-2xl font-semibold mb-4 pb-3 border-b">Experience</h2>
+        <div className="flex flex-wrap justify-between">
+          <div>
+            <h2 className="text-lg font-medium pb-1 md:text-xl self-start">Flutter Developer (Internship)</h2>
+            <p className="w-full text-md text-gray-500 md:text-lg self-end">PT. PJB UBJOM PLTU Rembang - Rembang, Indonesia</p>
+          </div>
+          <p className="text-sm text-gray-500 md:text-lg">
+            Sep-Nov 2021
+          </p>
+        </div>
       </section>
 
     </div>
--- lib/static.tsx
+++ lib/static.tsx
@@ -1,7 +1,7 @@
 import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const socials = [
-  { label: "GitHub", icon: <FaGithub size={28} />, href: "https://github.com/aktssam" },
-  { label: "LinkedIn", icon: <FaLinkedin size={28} />, href: "https://linkedin.com/in/aktssam" },
-  { label: "Email", icon: <FaEnvelope size={28} />, href: "mailto:aktssam.alfarisi@gmail.com" }
+  { label: "GitHub", icon: <FaGithub size={21} />, href: "https://github.com/aktssam" },
+  { label: "LinkedIn", icon: <FaLinkedin size={21} />, href: "https://linkedin.com/in/aktssam" },
+  { label: "Email", icon: <FaEnvelope size={21} />, href: "mailto:aktssam.alfarisi@gmail.com" }
 ]
